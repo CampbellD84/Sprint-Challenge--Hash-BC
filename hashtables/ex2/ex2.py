33b2a2ffc5e2f7507b1cd87212d975ded61bb3c4
Douglas Campbell <dacampbelltbone@gmail.com>
2020-03-21 13:45:39 -0400

    Completed hashtables exercise 2. Ran tests and all tests have passed.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -16,8 +14,16 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # Fill the Hashtable with ticket info (src, dest)
+    for tkt in tickets:
+        src = tkt.source
+        dest = tkt.destination
+        hash_table_insert(hashtable, src, dest)
 
-    pass
+    # Setting up flight itinerary
+    itinerary = hash_table_retrieve(hashtable, "NONE")
+    for i in range(length):
+        route[i] = itinerary
+        itinerary = hash_table_retrieve(hashtable, itinerary)
+
+    return route[:-1]
